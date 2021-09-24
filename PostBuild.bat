ab033550ad8e4e3f4751aae709d0547a263995e6
WesternGamer <80211714+WesternGamer@users.noreply.github.com>
2021-09-24 13:22:36 -0400

    Add files via upload

---
+++ PostBuild.bat
@@ -0,0 +1,4 @@
+copy "D:\VS2019Projects\InstantExit\bin\Release\InstantExit.dll" "D:\Games\steamapps\common\SpaceEngineers\Bin64\Plugins"
+C:
+cd "C:\Program Files (x86)\Steam"
+steam steam://rungameid/244850
