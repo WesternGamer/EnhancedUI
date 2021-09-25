ab877e096fbfaa14e4b38045f79fe7fbee952b10
z__ <vinduus585@gmail.com>
2021-09-25 13:16:36 +0700

    postbuild process changes
    because i have different paths, so just use variables from msbuild

--- PostBuild.bat
+++
@@ -1,4 +0,0 @@
-copy "D:\VS2019Projects\EnhancedUI\EnhancedUI\bin\net48\" "D:\Games\steamapps\common\SpaceEngineers\Bin64\Plugins\Local\EnhancedUI"
-C:
-cd "C:\Program Files (x86)\Steam"
-steam steam://rungameid/244850
