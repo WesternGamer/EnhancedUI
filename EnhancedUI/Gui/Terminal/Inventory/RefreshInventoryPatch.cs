60e551223c2270340cd5d062e3a1022aad4427f2
WesternGamer <kevnguyen474@gmail.com>
2021-09-26 19:15:25 -0400

    Patched Inventory menu of terminal to make web pages work on it.

---
+++ EnhancedUI/Gui/Terminal/Inventory/RefreshInventoryPatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+
+namespace EnhancedUI.Gui.Terminal.Inventory
+{
+    [HarmonyPatch]
+    // ReSharper disable once UnusedType.Global
+    internal static class RefreshInventoryPatch
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(Type.GetType("Sandbox.Game.Gui.MyTerminalInventoryController, Sandbox.Game", true), "Refresh");
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        private static bool Prefix() => false;
+    }
+}
