d11889d164bd0ce4f234a443aa28a87e9cca8ea7
WesternGamer <kevnguyen474@gmail.com>
2021-11-03 21:45:06 -0400

    Disabled original background video as it is planned for the video to be on the background of the webpage.

---
+++ EnhancedUI/Gui/MainMenu/MyGuiScreenMainMenuBase_CloseScreenNow_Patch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using SpaceEngineers.Game.GUI;
+
+namespace EnhancedUI.Gui.MainMenu
+{
+    /// <summary>
+    /// This patch is to prevent exceptions at runtime when MyGuiScreenMainMenu.AddIntroScreen is patched by MyGuiScreenMainMenu_AddIntroScreen_Patch.
+    /// </summary>
+    [HarmonyPatch(typeof(MyGuiScreenMainMenu), "CloseScreenNow")]
+    internal class MyGuiScreenMainMenuBase_CloseScreenNow_Patch
+    {
+        private static bool Prefix() => false;
+    }
+}
