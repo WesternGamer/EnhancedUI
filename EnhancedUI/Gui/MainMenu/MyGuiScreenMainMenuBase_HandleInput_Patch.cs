135fc2debc3c6be8b76ff7a90dc3224003f75215
WesternGamer <kevnguyen474@gmail.com>
2021-11-03 21:34:39 -0400

    Removed original input as it is expected that everything will be done in the web page.

---
+++ EnhancedUI/Gui/MainMenu/MyGuiScreenMainMenuBase_HandleInput_Patch.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using Sandbox.Game.Screens;
+
+namespace EnhancedUI.Gui.MainMenu
+{
+    [HarmonyPatch(typeof(MyGuiScreenMainMenuBase), "HandleInput")]
+    internal class MyGuiScreenMainMenuBase_HandleInput_Patch
+    {
+        private static bool Prefix() => false;
+    }
+}
