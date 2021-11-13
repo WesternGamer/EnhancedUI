a5e42d6734c6e9d825b87dd3c1969b10ab448bc9
WesternGamer <kevnguyen474@gmail.com>
2021-11-12 22:54:36 -0500

    Progress update.

--- EnhancedUI/Content/MainMenu.js
+++ EnhancedUI/Content/MainMenu.js
@@ -1,15 +1,14 @@
-// Invoked from C#
-// noinspection JSUnusedGlobalSymbols
-function Exit() {
-    window.Exit();
-}
-
-export const startCefSharp = async () => {
-    console.log("We are bound");
-    await CefSharp.BindObjectAsync("WebPageViewModel");
-    const { WebPageViewModel.Exit() } = window;
-}
-
 let {
     CefSharp,
-} = window;
+} = window; // this is equivalent to let CefSharp = window.CefSharp
+
+let WebPageViewModel;
+
+const startCefSharp = async () => {
+    console.log("We are bound");
+    await CefSharp.BindObjectAsync("WebPageViewModel");
+    const { WebPageViewModel } = window;
+}
+
+startCefSharp()
+    .then(() => { WebPageViewModel.Exit() })
